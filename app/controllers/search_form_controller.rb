49f82550f3444c8feab975b84095d4580ab97414
Chris Tam <christophertam@brandeis.edu>
2021-04-09 11:22:21 -0400

    update git

---
+++ app/controllers/search_form_controller.rb
@@ -0,0 +1,2 @@
+class SearchFormController < ApplicationController
+end
---
+++ test/controllers/search_form_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SearchFormControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
